76a3d06bcae6190be53998a54cce57baa32cf4b2
DanyFlow25 <122042411@upq.edu.mx>
2024-05-22 20:43:14 -0600

    Finalicé Práctica 3 FLASK

--- Práctica 1/OverFlowError/Clase.py
+++ Práctica 1/OverFlowError/Clase.py
@@ -2,14 +2,15 @@ import tkinter.messagebox as messagebox
 
 class Overflow:
     def __init__(self):
-        self.valor = 0
+        self.valor = int
 
     def operacion(self):
         try:
-            resultado = self.valor ** 99999
+            resultado = self.valor ** 999999
             return resultado
         except OverflowError:
             messagebox.showerror("Error Overflow", "Error Overflow (Cantidad demasiado grande)")
 
     def establecer_valor(self, valor):
         self.valor = valor
+        

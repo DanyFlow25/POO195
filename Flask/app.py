60f4f06a020d2ed1b556ad5afaf3e5cfcd015bd7
DanyFlow25 <122042411@upq.edu.mx>
2024-05-27 20:13:01 -0600

    Rutas con Flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,35 @@
-from flask import Flask 
+from flask import Flask, request 
 
 app= Flask(__name__)
+
+# Ruta Simple
+@app.route('/')
+def principal():
+    return 'Hola Mundo Flask'
+
+# Ruta doble
+@app.route('/usuario')
+@app.route('/saludar')
+def saludos():
+    return 'Hola Daniel Antonio'
+
+# Rutas con parámetros
+@app.route('/hi/<nombre>')
+def hi(nombre):
+    return 'Hola' + nombre + '!!!'
+
+# Definición de métodos de trabajo
+@app.route('/formulario', methods = ['GET', 'POST'] )
+def formulario():
+    if request.method == 'GET':
+        return 'No es seguro enviar password por GET'
+    elif request.method == 'POST':
+        return 'POST si es seguro para passwords'
+
+# Manejo de excepciones
+@app.errorhandler(404)
+def paginano(e):
+    return 'Revisa tu sintaxis: No encontré nada'
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)

fe874476b56c9d9e626ace37e8522423590f4d3f
DanyFlow25 <122042411@upq.edu.mx>
2024-06-10 20:29:20 -0600

    Finalicé práctica 5

--- CRUD Albums/app.py
+++ CRUD Albums/app.py
@@ -1,24 +1,29 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'bdflask'
 mysql = MySQL(app)
-
+app.secret_key = 'mysecretkey'
 @app.route('/')
 def index():
     return render_template('index.html')
 @app.route('/guardarAlbum', methods=['POST'])
 def guardarAlbum():
     if request.method == 'POST':
-        titulo = request.form['txtTitulo']
-        artista = request.form['txtArtista']
-        anio = request.form['txtAnio']
-        print(titulo,artista,anio)
-        return 'Datos recibidos en el server'
+        Ftitulo = request.form['txtTitulo']
+        Fartista = request.form['txtArtista']
+        Fanio = request.form['txtAnio']
+
+        #Enviamos a la BD
+        cursor = mysql.connection.cursor()
+        cursor.execute('insert into albums(titulo, artista, anio) values(%s,%s,%s)', (Ftitulo,Fartista,Fanio))
+        mysql.connection.commit()
+        flash('Album Guardado Correctamente')        
+        return redirect(url_for('index'))
 
 # Manejo de excepciones
 @app.errorhandler(404)
